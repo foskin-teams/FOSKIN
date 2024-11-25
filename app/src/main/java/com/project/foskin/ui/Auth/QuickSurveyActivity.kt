85d441aa7f9391dadd64f0d9b9843c2f81cbcf41
Marcello <marcelloilham54@gmail.com>
2024-11-25 13:29:22 +0700

    update
    delete top padding main activity

--- app/src/main/java/com/project/foskin/ui/Auth/QuickSurveyActivity.kt
+++ app/src/main/java/com/project/foskin/ui/Auth/QuickSurveyActivity.kt
@@ -19,6 +19,9 @@ class QuickSurveyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_quick_survey)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
