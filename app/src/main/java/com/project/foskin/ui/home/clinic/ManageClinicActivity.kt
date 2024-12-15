44e57430b297e8bb18b6569e95a8148a378455b9
Marcello <marcelloilham54@gmail.com>
2024-12-15 22:16:03 +0700

    update
    add page manage clinic

---
+++ app/src/main/java/com/project/foskin/ui/home/clinic/ManageClinicActivity.kt
@@ -0,0 +1,19 @@
+package com.project.foskin.ui.home.clinic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.foskin.R
+
+class ManageClinicActivity : AppCompatActivity() {
+
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
