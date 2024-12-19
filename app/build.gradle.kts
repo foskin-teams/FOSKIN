5626e185906a27559a19964cd8ce06341b48beaf
Marcello <marcelloilham54@gmail.com>
2024-12-19 19:20:02 +0700

    update
    add voice to text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
+    implementation("androidx.appcompat:appcompat:1.3.1")
+
 }
